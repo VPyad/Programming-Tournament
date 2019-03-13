be90954d85f7b672163f2a0cbe7ff50efae76fc5
Vadim Pyadukhov <vpyadukhov@outlook.com>
2019-03-13 21:08:37 +0300

    add lecturer and student applications models and create migration

---
+++ Programming-Tournament/Areas/Identity/Models/LecturerApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming_Tournament.Areas.Identity.Models
+{
+    public class LecturerApplication
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long LecturerApplicationId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int? DocNo { get; set; }
+
+        public Faculty Faculty { get; set; }
+
+        public Lectern Lectern { get; set; }
+    }
+}
---
+++ Programming-Tournament/Areas/Identity/Models/StudentApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming_Tournament.Areas.Identity.Models
+{
+    public class StudentApplication
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long StudentApplicationId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int? DocNo { get; set; }
+
+        public Faculty Faculty { get; set; }
+
+        public Lectern Lectern { get; set; }
+
+        public DegreeType DegreeType { get; set; }
+
+        public Сurriculum Сurriculum { get; set; }
+    }
+}
