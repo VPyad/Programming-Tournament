0140154ff6236d4c596013083535a9cdd4f942a2
Vadim Pyadukhov <vpyadukhov@outlook.com>
2019-03-16 12:55:52 +0300

    Add login/logout

---
+++ Programming-Tournament/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Programming_Tournament.Areas.Identity.Models;
+
+namespace Programming_Tournament.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LogoutModel> logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await signInManager.SignOutAsync();
+            logger.LogInformation("User logged out.");
+
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
