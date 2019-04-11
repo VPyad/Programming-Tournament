f63426be4f9b497c6f7e804633cabc3bef7eed4e
Vadim Pyadukhov <vpyadukhov@outlook.com>
2019-04-11 17:22:28 +0300

    add layout to change lang, add post handler, lang do not changes

--- Programming-Tournament/Areas/Identity/Pages/Manage/Index.cshtml.cs
+++ Programming-Tournament/Areas/Identity/Pages/Manage/Index.cshtml.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 using Programming_Tournament.Areas.Admin.Models;
 using Programming_Tournament.Areas.Identity.Models;
 using Programming_Tournament.Data;
@@ -18,11 +24,13 @@ namespace Programming_Tournament.Areas.Identity.Pages.Manage
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly ApplicationDbContext context;
+        private readonly IOptions<RequestLocalizationOptions> localizationOptions;
 
-        public IndexModel(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        public IndexModel(UserManager<ApplicationUser> userManager, ApplicationDbContext context, IOptions<RequestLocalizationOptions> localizationOptions)
         {
             this.context = context;
             this.userManager = userManager;
+            this.localizationOptions = localizationOptions;
         }
 
         [TempData]
@@ -60,6 +68,22 @@ namespace Programming_Tournament.Areas.Identity.Pages.Manage
             else
                 Input = new InputModel(user);
 
+            var cultures = localizationOptions.Value.SupportedUICultures.ToList();
+            var rqf = Request.HttpContext.Features.Get<IRequestCultureFeature>();
+            var currentCulture = rqf.RequestCulture.Culture;
+
+            List<SelectListItem> langs = new List<SelectListItem>();
+
+            foreach (var item in cultures)
+            {
+                var code = item.TwoLetterISOLanguageName;
+                var name = code == "en" ? "English" : "Русский";
+                langs.Add(new SelectListItem { Text = name, Value = code, Selected = code == currentCulture.TwoLetterISOLanguageName });
+            }
+
+            Input.LangsSelectList = langs;
+            Input.LangCode = langs.FirstOrDefault(x => x.Selected).Value;
+
             return Page();
         }
 
@@ -80,6 +104,21 @@ namespace Programming_Tournament.Areas.Identity.Pages.Manage
 
             await OnGetAsync();
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostChangeLang()
+        {
+            var cultures = localizationOptions.Value.SupportedUICultures.ToList();
+            var culture = cultures.FirstOrDefault(x => x.TwoLetterISOLanguageName == Input.LangCode);
+
+            if (culture != null)
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+            return await OnGetAsync();
         }
     }
 
@@ -106,5 +145,10 @@ namespace Programming_Tournament.Areas.Identity.Pages.Manage
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
+
+        [Display(Name = "Change language")]
+        public IEnumerable<SelectListItem> LangsSelectList { get; set; }
+
+        public string LangCode { get; set; }
     }
 }
