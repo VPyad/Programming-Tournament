ec1680e4b4fae7dacb1de2f437d644786f6829e3
Vadim Pyadukhov <vpyadukhov@outlook.com>
2019-04-11 16:33:33 +0300

    add translation to home, about pages and toolbar

--- Programming-Tournament/Startup.cs
+++ Programming-Tournament/Startup.cs
@@ -14,6 +14,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Programming_Tournament.Areas.Identity.Models;
 using Programming_Tournament.Models.Domain.User;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace Programming_Tournament
 {
@@ -43,12 +46,51 @@ namespace Programming_Tournament
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => {
+                var F = services.BuildServiceProvider().GetService<IStringLocalizerFactory>();
+                var L = F.Create("ModelBindingMessages", "Programming-Tournament");
+                options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+                    (x) => L["The value '{0}' is invalid."]);
+                options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(
+                    (x) => L["The field {0} must be a number."]);
+                options.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(
+                    (x) => L["A value for the '{0}' property was not provided.", x]);
+                options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+                    (x, y) => L["The value '{0}' is not valid for {1}.", x, y]);
+                options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+                    () => L["A value is required."]);
+                options.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor(
+                    (x) => L["The supplied value is invalid for {0}.", x]);
+                options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(
+                    (x) => L["Null value is invalid.", x]);
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            .AddDataAnnotationsLocalization()
+            .AddViewLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("ru") };
+                options.DefaultRequestCulture = new RequestCulture("en", "en");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddLocalization(options => { options.ResourcesPath = "Resources"; });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+            var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("ru") };
+            app.UseRequestLocalization(new RequestLocalizationOptions()
+            {
+                DefaultRequestCulture = new RequestCulture(new CultureInfo("en")),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
